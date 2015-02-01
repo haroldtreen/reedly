ca36062415a7e66f5cc08935477e4d2ab440b25e
Harold Treen <haroldtreen@gmail.com>
2015-02-01 02:55:18 -0500

    Add book create and book generation before publish

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,20 +1,24 @@
 class BooksController < ApplicationController
+	skip_before_filter :verify_authenticity_token 
+
 	def index
 		render json: Book.all
 	end
 
 	def create
-		Book.create(book_params)
+		book = Book.create(title: book_params[:title], links: book_params[:links].join(";"), user_id: User.last.id)
+		render json: book
 	end
 
 	def publish
 		book = Book.find(params[:id])
+		Reedler.to_epub(book)
 		PublishService.send_to_kindle(book)
 	end
 
 	private 
 
 	def book_params
-		params.require(:book).permit(:title, :links, :description)
+		params.require(:book).permit(:title, { links: [] }, :description)
 	end
 end
